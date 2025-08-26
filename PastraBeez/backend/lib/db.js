5ae06388328370e9bc70a03b4676b85cb3420ade
Troy <97826350+Trill1um@users.noreply.github.com>
2025-08-26 16:20:36 +0800

    Log MongoDB URI on connection attempt

--- PastraBeez/backend/lib/db.js
+++ PastraBeez/backend/lib/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
+    console.log('MongoDB URI:', process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -15,4 +16,5 @@ export const connectDB = async () => {
     process.exit(1);
   }
 };
+
 
