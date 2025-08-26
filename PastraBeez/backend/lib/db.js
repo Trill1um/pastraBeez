fd357a668c71efaf038218f243802c3e8171b461
Troy <97826350+Trill1um@users.noreply.github.com>
2025-08-26 16:11:41 +0800

    Change MongoDB URI environment variable name

--- PastraBeez/backend/lib/db.js
+++ PastraBeez/backend/lib/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -15,3 +15,4 @@ export const connectDB = async () => {
     process.exit(1);
   }
 };
+
