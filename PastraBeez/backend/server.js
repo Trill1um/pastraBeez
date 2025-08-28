2977275670aead90d9a7951f83f6b32ce0db6a8c
Troy <97826350+Trill1um@users.noreply.github.com>
2025-08-28 20:51:41 +0800

    Update server.js

--- PastraBeez/backend/server.js
+++ PastraBeez/backend/server.js
@@ -37,7 +37,8 @@ app.use((req, res, next) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, "0.0.0.0.", () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     connectDB();
+
 });
